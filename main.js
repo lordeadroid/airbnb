823c52fdcb64818b4d0d835709dd9fd3e52e02cf
lordeadroid <rishabhrawat34@gmail.com>
2024-05-31 17:22:12 +0530

    refactored html and js

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-const removeStyle = (footerElements) => {
-  footerElements.forEach((element) => {
-    element.style.color = "#6a6b6b";
-  });
+const removeColor = (element) => {
+  element.style.removeProperty("color");
 };
 
-const changeColor = (containerElement) => {
+const resetStyleProperty = (elements, styler) => {
+  elements.forEach(styler);
+};
+
+const modifyColorOnClick = (containerElement) => {
+  // creating array from HTMLCollection
   const elements = Array.from(containerElement.children);
   const [firstElement] = elements;
   firstElement.style.color = "black";
 
   elements.forEach((element) => {
     element.onclick = (event) => {
-      removeStyle(elements);
-      event.srcElement.style.color = "black";
+      //resetting elements color
+      resetStyleProperty(elements, removeColor);
+      event.target.style.color = "black";
     };
   });
 };
@@ -21,8 +25,8 @@ const main = () => {
   const footerElements = document.querySelector(".categories");
   const optionElements = document.querySelector(".options");
 
-  changeColor(footerElements);
-  changeColor(optionElements);
+  modifyColorOnClick(footerElements);
+  modifyColorOnClick(optionElements);
 };
 
 window.onload = main;
